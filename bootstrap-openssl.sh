26ca93a2638d09a78f76e13f3c4dc3dd22e78a72
William KÖHLER <williamkoehler0@gmail.com>
2022-03-09 20:13:23 +0000

    Update OpenSSL Version

--- bootstrap-openssl.sh
+++ bootstrap-openssl.sh
@@ -1,10 +1,10 @@
 # Download and extract openssl
 mkdir openssl
-wget "https://www.openssl.org/source/openssl-3.0.1.tar.gz"
-tar -x -f "openssl-3.0.1.tar.gz" --overwrite -C "openssl"
+wget "https://www.openssl.org/source/openssl-1.1.1m.tar.gz"
+tar -x -f "openssl-1.1.1m.tar.gz" --overwrite -C "openssl"
 
 # Configure openssl
-cd openssl/openssl-3.0.1
+cd "openssl/openssl-1.1.1m"
 ./config --openssldir=/usr/local/ssl
 
 # Compile openssl

77b9e57dd5d7b1ebe344c084ffcb96e5462993bd
William Koehler <williamkoehler0@gmail.com>
2023-04-10 22:03:37 +0200

    Removed callback property.

--- server/home-scripting-native/utils/Property.hpp
+++ server/home-scripting-native/utils/Property.hpp
@@ -11,16 +11,8 @@ namespace server
         {
             class NativeScriptImpl;
 
-            template <typename P = void*, class T = NativeScriptImpl>
-            using PropertyGetterDefinition = P (T::*)() const;
-
-            template <typename P = void*, class T = NativeScriptImpl>
-            using PropertySetterDefinition = void (T::*)(const P&);
-
-            template <typename P, class T>
-            class PropertyCallbackImpl;
-            template <typename P, class T>
-            class PropertyReadonlyCallbackImpl;
+            template <class T = NativeScriptImpl>
+            using PropertyUpdateDefinition = void (T::*)();
 
             template <typename P, class T>
             class PropertyReferenceImpl;
@@ -44,32 +36,15 @@ namespace server
                 ///
                 /// @tparam P Property value type
                 /// @tparam T Script type
-                /// @param getter Property getter method
-                /// @param setter Property setter method
+                /// @param property Property reference
                 /// @param propertyFlags Property flags
                 /// @return UniqueRef<Property> Property
                 template <typename P, class T>
-                static UniqueRef<Property> Create(PropertyGetterDefinition<P, T> getter,
-                                                  PropertySetterDefinition<P, T> setter,
-                                                  uint8_t propertyFlags = PropertyFlags::kPropertyFlag_Visible)
+                static inline UniqueRef<Property> Create(P T::*property, PropertyUpdateDefinition<T> updateCallback,
+                                                         uint8_t propertyFlags = PropertyFlags::kPropertyFlag_Visible)
                 {
-                    assert(getter != nullptr);
-                    return boost::make_unique<PropertyCallbackImpl<P, T>>(getter, setter, propertyFlags);
-                }
-
-                /// @brief Create readonly property
-                ///
-                /// @tparam P Property value type
-                /// @tparam T Script type
-                /// @param getter Property getter method
-                /// @param propertyFlags Property flags
-                /// @return UniqueRef<Property> Property
-                template <typename P, class T>
-                static UniqueRef<Property> CreateReadonly(PropertyGetterDefinition<P, T> getter,
-                                                          uint8_t propertyFlags = PropertyFlags::kPropertyFlag_Visible)
-                {
-                    assert(getter != nullptr);
-                    return boost::make_unique<PropertyReadonlyCallbackImpl<P, T>>(getter, propertyFlags);
+                    assert(property != nullptr);
+                    return boost::make_unique<PropertyReferenceImpl<P, T>>(property, updateCallback, propertyFlags);
                 }
 
                 /// @brief Create property
@@ -80,11 +55,10 @@ namespace server
                 /// @param propertyFlags Property flags
                 /// @return UniqueRef<Property> Property
                 template <typename P, class T>
-                static UniqueRef<Property> Create(P T::*property,
-                                                  uint8_t propertyFlags = PropertyFlags::kPropertyFlag_Visible)
+                static inline UniqueRef<Property> Create(P T::*property,
+                                                         uint8_t propertyFlags = PropertyFlags::kPropertyFlag_Visible)
                 {
-                    assert(property != nullptr);
-                    return boost::make_unique<PropertyReferenceImpl<P, T>>(property, propertyFlags);
+                    return Create<P, T>(property, nullptr, propertyFlags);
                 }
 
                 /// @brief Create readonly property
@@ -119,109 +93,20 @@ namespace server
                 virtual void Set(void* self, const Value& value) = 0;
             };
 
-#define PROPERTY_GETTER (static_cast<T*>(self)->*getter)
-#define PROPERTY_SETTER (static_cast<T*>(self)->*setter)
-
-#define PROPERTY_CALLBACK_IMPLEMENTATION(type, valueType, getterExpr, setterCondition, setterExpr)                     \
-    template <class T>                                                                                                 \
-    class PropertyCallbackImpl<type, T> final : public Property                                                        \
-    {                                                                                                                  \
-      private:                                                                                                         \
-        PropertyGetterDefinition<type, T> getter;                                                                      \
-        PropertySetterDefinition<type, T> setter;                                                                      \
-                                                                                                                       \
-      public:                                                                                                          \
-        PropertyCallbackImpl<type, T>(PropertyGetterDefinition<type, T> getter,                                        \
-                                      PropertySetterDefinition<type, T> setter, uint8_t propertyFlags)                 \
-            : Property(propertyFlags), getter(getter), setter(setter)                                                  \
-        {                                                                                                              \
-            assert(getter != nullptr);                                                                                 \
-            assert(setter != nullptr);                                                                                 \
-        }                                                                                                              \
-        virtual ValueType GetType() const override                                                                     \
-        {                                                                                                              \
-            return valueType;                                                                                          \
-        }                                                                                                              \
-        virtual Value Get(void* self) const override                                                                   \
-        {                                                                                                              \
-            return getterExpr;                                                                                         \
-        }                                                                                                              \
-        virtual void Set(void* self, const Value& value) override                                                      \
-        {                                                                                                              \
-            if (setterCondition && setter != nullptr)                                                                  \
-                setterExpr;                                                                                            \
-        }                                                                                                              \
-    };                                                                                                                 \
-                                                                                                                       \
-    template <class T>                                                                                                 \
-    class PropertyReadonlyCallbackImpl<type, T> final : public Property                                                \
-    {                                                                                                                  \
-      private:                                                                                                         \
-        PropertyGetterDefinition<type, T> getter;                                                                      \
-                                                                                                                       \
-      public:                                                                                                          \
-        PropertyReadonlyCallbackImpl<type, T>(PropertyGetterDefinition<type, T> getter, uint8_t propertyFlags)         \
-            : Property(propertyFlags), getter(getter)                                                                  \
-        {                                                                                                              \
-            assert(getter != nullptr);                                                                                 \
-        }                                                                                                              \
-        virtual ValueType GetType() const override                                                                     \
-        {                                                                                                              \
-            return valueType;                                                                                          \
-        }                                                                                                              \
-        virtual Value Get(void* self) const override                                                                   \
-        {                                                                                                              \
-            return getterExpr;                                                                                         \
-        }                                                                                                              \
-        virtual void Set(void* self, const Value& value) override                                                      \
-        {                                                                                                              \
-            (void)self;                                                                                                \
-            (void)value;                                                                                               \
-        }                                                                                                              \
-    };
-
-            PROPERTY_CALLBACK_IMPLEMENTATION(bool, ValueType::kBooleanType, Value(PROPERTY_GETTER()), value.IsBoolean(),
-                                             PROPERTY_SETTER(value.GetBoolean()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(double_t, ValueType::kNumberType, Value(PROPERTY_GETTER()),
-                                             value.IsNumber(), PROPERTY_SETTER(value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(size_t, ValueType::kNumberType, Value((const double_t&)PROPERTY_GETTER),
-                                             value.IsNumber(), PROPERTY_SETTER((size_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(int8_t, ValueType::kNumberType, Value((const int8_t&)PROPERTY_GETTER()),
-                                             value.IsNumber(), PROPERTY_SETTER((int8_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(uint8_t, ValueType::kNumberType, Value((const uint8_t&)PROPERTY_GETTER()),
-                                             value.IsNumber(), PROPERTY_SETTER((uint8_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(int16_t, ValueType::kNumberType, Value((const int16_t&)PROPERTY_GETTER()),
-                                             value.IsNumber(), PROPERTY_SETTER((int16_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(uint16_t, ValueType::kNumberType,
-                                             Value((const uint16_t&)PROPERTY_GETTER()), value.IsNumber(),
-                                             PROPERTY_SETTER((uint16_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(int32_t, ValueType::kNumberType, Value((const int32_t&)PROPERTY_GETTER()),
-                                             value.IsNumber(), PROPERTY_SETTER((int32_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(uint32_t, ValueType::kNumberType,
-                                             Value((const uint32_t&)PROPERTY_GETTER()), value.IsNumber(),
-                                             PROPERTY_SETTER((uint32_t)value.GetNumber()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(std::string, ValueType::kStringType, Value(PROPERTY_GETTER()),
-                                             value.IsString(), PROPERTY_SETTER(value.GetString()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(Endpoint, ValueType::kEndpointType, Value(PROPERTY_GETTER()),
-                                             value.IsEndpoint(), PROPERTY_SETTER(value.GetEndpoint()))
-            PROPERTY_CALLBACK_IMPLEMENTATION(Color, ValueType::kColorType, Value(PROPERTY_GETTER()), value.IsColor(),
-                                             PROPERTY_SETTER(value.GetColor()))
-
-#undef PROPERTY_GETTER
-#undef PROPERTY_SETTER
-#undef PROPERTY_CALLBACK_IMPLEMENTATION
-
 #define PROPERTY_REFERENCE (static_cast<T*>(self)->*reference)
+
 #define PROPERTY_REFERENCE_IMPLEMENTATION(type, valueType, getterExpr, setterCondition, setterExpr)                    \
     template <class T>                                                                                                 \
     class PropertyReferenceImpl<type, T> : public Property                                                             \
     {                                                                                                                  \
       private:                                                                                                         \
         type T::*reference;                                                                                            \
+        PropertyUpdateDefinition<T> updateMethod;                                                                      \
                                                                                                                        \
       public:                                                                                                          \
-        PropertyReferenceImpl<type, T>(type T::*reference, uint8_t propertyFlags)                                      \
-            : Property(propertyFlags), reference(reference)                                                            \
+        PropertyReferenceImpl<type, T>(type T::*reference, PropertyUpdateDefinition<T> updateMethod,                   \
+                                       uint8_t propertyFlags)                                                          \
+            : Property(propertyFlags), reference(reference), updateMethod(updateMethod)                                \
         {                                                                                                              \
             assert(reference != nullptr);                                                                              \
         }                                                                                                              \
@@ -236,7 +121,11 @@ namespace server
         virtual void Set(void* self, const Value& value) override                                                      \
         {                                                                                                              \
             if (setterCondition)                                                                                       \
+            {                                                                                                          \
                 setterExpr;                                                                                            \
+                if (updateMethod != nullptr)                                                                           \
+                    (static_cast<T*>(self)->*updateMethod)();                                                            \
+            }                                                                                                          \
         }                                                                                                              \
     };                                                                                                                 \
                                                                                                                        \
